f6fa38d0f7164337c115c4b0daaabbab1c92a8a9
melozhe <melozhe.huang@gmail.com>
2018-12-11 13:14:58 -0800

    c

--- boxVR/Assets/collision.cs
+++ boxVR/Assets/collision.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class collision : MonoBehaviour {
+public class collision : MonoBehaviour
+{
     public static int globalScore;
     public static int globalLives = 20;
     public static int globalStreaks;
     public AudioClip hapticAudioClip;
-  
+    public Material[] Materials;
+    public Renderer rend;
+    public Material[] Materials1;
+    public Renderer rend1;
 
     void Start()
     {
-       // globalScore = score;
+        // globalScore = score;
+        rend = GetComponent<Renderer>();
+        rend1 = GetComponent<Renderer>();
     }
     void OnCollisionEnter(Collision col)
     {
@@ -27,12 +33,38 @@ public class collision : MonoBehaviour {
             if (col.collider.name == "RightHandAnchor")
                 OVRHaptics.RightChannel.Preempt(hapticsClip);
         }
-        else
+        if (col.collider.tag == "destruct")
         {
             Debug.Log("dropped!!");
             Destroy(gameObject);
             globalLives--;
             globalStreaks = 0;
+        }
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Note: we use colliders here, not collisions
+        if (other.gameObject.tag == "wall")
+        {
+            Debug.Log("color change");
+            Material[] mats = rend.materials;
+            mats[0] = Materials[0];
+            rend.materials = mats;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        //Note: we use colliders here, not collisions
+        if (other.gameObject.tag == "wall")
+        {
+            //Debug.Log("color change");
+            Material[] mats1 = rend1.materials;
+            mats1[0] = Materials1[0];
+            rend1.materials = mats1;
         }
     }
 }
